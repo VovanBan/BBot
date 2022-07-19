e354f708489e74dfebce78b8978140ae23931db2
klewreole <107884081+klewreole@users.noreply.github.com>
2022-07-20 01:27:33 +0500

    Update main.py

--- BusinessBot/main.py
+++ BusinessBot/main.py
@@ -1,6 +1,9 @@
 from bots.memabot import main_memabot
 from bots.memabotedit import main_memabotedit
 
+async def main():
+    await main_memabot()
+    await main_memabotedit()
+
 if __name__ == '__main__':
-    main_memabot()
-    main_memabotedit()
+    main()
