c45018e966476cf0bf1954ce2e15398e8cf142d3
klewreole <107884081+klewreole@users.noreply.github.com>
2022-08-15 10:34:40 +0500

    Delete main.py

--- BusinessBot/main.py
+++
@@ -1,7 +0,0 @@
-import threading
-from bots.memabot import main_memabot
-from bots.memabotedit import main_memabotedit
-
-if __name__ == '__main__':
-    threading.Thread(target=main_memabot()).start()
-    threading.Thread(target=main_memabotedit()).start()
